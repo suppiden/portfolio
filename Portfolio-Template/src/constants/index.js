2c789b8a74a277720508cf4a01da045ad9a86ba2
suppiden <jonathansantos.vae@gmail.com>
2024-09-24 22:59:51 +0200

    Portfolio

---
+++ Portfolio-Template/src/constants/index.js
@@ -0,0 +1,270 @@
+import {
+  frontend,
+  backend,
+  ux,
+  prototyping,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  git,
+  figma,
+  docker,
+  postgresql,
+  rubyrails,
+  graphql,
+  komikult,
+  leaderboard,
+  math,
+  movie,
+  nyeusi,
+  coverhunt,
+  dcc,
+  kelhel,
+  microverse,
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'Frontend Developer',
+    icon: frontend,
+  },
+  {
+    title: 'Backend Developer',
+    icon: backend,
+  },
+  {
+    title: 'UI/UX Design',
+    icon: ux,
+  },
+  {
+    title: 'Software Prototyping',
+    icon: prototyping,
+  },
+];
+
+const technologies = [
+  {
+    name: 'HTML 5',
+    icon: html,
+  },
+  {
+    name: 'CSS 3',
+    icon: css,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'TypeScript',
+    icon: typescript,
+  },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Redux Toolkit',
+    icon: redux,
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: tailwind,
+  },
+  {
+    name: 'Node JS',
+    icon: nodejs,
+  },
+  {
+    name: 'Rails',
+    icon: rubyrails,
+  },
+  {
+    name: 'graphql',
+    icon: graphql,
+  },
+  {
+    name: 'postgresql',
+    icon: postgresql,
+  },
+  {
+    name: 'git',
+    icon: git,
+  },
+  {
+    name: 'figma',
+    icon: figma,
+  },
+  {
+    name: 'docker',
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: 'Trainee Developer',
+    company_name: 'Sagitaz',
+    icon: coverhunt,
+    iconBg: '#333333',
+    date: 'Mar 2024 - Jun 2024',
+  },
+  {
+    title: 'Legal back office',
+    company_name: 'Lexer',
+    icon: microverse,
+    iconBg: '#333333',
+    date: 'Mar 2022 - May 2022',
+  },
+  {
+    title: 'Legal back office',
+    company_name: 'DXC',
+    icon: kelhel,
+    iconBg: '#333333',
+    date: 'May 2022 - Oct 2022',
+  },
+  {
+    title: 'Administrative',
+    company_name: 'Cerciora',
+    icon: dcc,
+    iconBg: '#333333',
+    date: 'Sep 2022 - Present',
+  },
+];
+
+const projects = [
+  {
+    id: 'project-1',
+    name: 'FitAI',
+    description: 'A web where you hire an AI as your personal trainer',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'mongodb',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'tailwind',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: komikult,
+    repo: 'https://github.com/suppiden/FitAi',
+    demo: '',
+  },
+  {
+    id: 'project-2',
+    name: 'APP-DWES',
+    description:
+      'A with user authentication, upload and download of files and session management with php',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'restapi',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'scss',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: leaderboard,
+    repo: 'https://github.com/RSMKode/app-dwes',
+    demo: '',
+  },
+  {
+    id: 'project-3',
+    name: 'Scrapper in Go',
+    description: 'A web scrapper to test Golang',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: math,
+    repo: 'https://github.com/suppiden/scrapper',
+    demo: '',
+  },
+  {
+    id: 'project-4',
+    name: 'Snake game ',
+    description: `Replica of the famous mobile game of the snake built in java, unfinished`,
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: movie,
+    repo: 'https://github.com/suppiden/Hilos',
+    demo: '',
+  },
+  {
+    id: 'project-5',
+    name: 'Biblioteca',
+    description:
+      'A demo of the inner works of a library built with Laravel and bootstrap',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: nyeusi,
+    repo: 'https://github.com/suppiden/biblioteca',
+    demo: '',
+  },
+];
+
+export { services, technologies, experiences, projects };
